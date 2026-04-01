6c98715d21a35c594fea1d38d86d9609a64a549f
Curso <Curso@Lab7-30.Ensino.lib>
2026-04-01 17:17:26 -0300

    Adicionando as anotações da aula de hoje e a proposta de projetinho desenvolvida com o objetivo de trabalhar com as funções propostas nos exercícios do curso. - (Já adicionei mais algumas funções no arquivo ContagemDeCaracteres.py mas foi no PC da universidade, onde não consegui realizar o commit (Vou adicionar as outras funções e alterar em casa))

---
+++ ExerciciosIniciais/ComparacaoDeDesempenho.py
@@ -0,0 +1,13 @@
+"""
+|| -------------------------- Comparação de desempenho ------------------------                                     
+|| Peça para o usuário digitar 2 listas de números inteiros (1-100), como pontuações de 2 jogadores                 
+|| De a opção de retirar os números repetidos.                                                                     
+|| Calcule a média de todos os números restantes.                                                                   
+|| Mostre as duas listas com os valores intercalados, apontando os que estavam maior que a média.                   
+|| Continue caso alguém ainda tenha pontuações.                                                                     
+|| Cada valor acima da média conta um ponto que, ao final, indica um vencedor                                       
+
+|| Autor: Vinícius Fagundes
+|| Data: 01/04/2026
+"""
+#This project is gonna be in english
