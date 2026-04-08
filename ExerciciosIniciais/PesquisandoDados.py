b4d7b1375a4751e84c897824ad1024da22c50e7e
Curso <Curso@Lab7-30.Ensino.lib>
2026-04-08 17:31:24 -0300

    Adicionando os arquivos da aula de hoje - Diconarios.txt tem um pouco das explicações mas continua incompleto (aula foi muito curta). PesquisandoDados.py fica com a proposta de exercício do curso

---
+++ ExerciciosIniciais/PesquisandoDados.py
@@ -0,0 +1,14 @@
+""" #Hoje vou fazer o exercício proposto pelo curso mesmo, devido ao tempo
+
+Leia o nome,sexo e idade de várias pessoas, salvando cada uma em um dicionário e todos os dicionários em uma lista.    ||   Read the name, gender, and age of several people, storing each one in a dictionary and all the dictionaries in a list.
+Mostre:                                                                                                                ||   Display: 
+- Quantas pessoas foram cadastradas                                                                                    ||   - How many people were registered         
+- A média de idade                                                                                                     ||   - The average age   
+- Listas com as mulherres                                                                                              ||   - Lists of women  
+- Lista de pessoas com a idade acima da média                                                                          ||   - List of people older than the average age 
+
+Autor: Vinícius Fagundes                                                                                               ||   Author: Vinícius Fagundes                                                                                                  
+Data: 08/04/2026                                                                                                       ||   Date: 04/08/2026 
+
+"""
+    
