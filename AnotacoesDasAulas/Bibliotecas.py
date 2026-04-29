98ea5250d45369ec0338fe3d16a1bbda06d4c0a5
Curso <Curso@Lab7-30.Ensino.lib>
2026-04-29 16:57:41 -0300

    Adicionando algumas anotações da aula de hoje (curta e teórica)

---
+++ AnotacoesDasAulas/Bibliotecas.py
@@ -0,0 +1,17 @@
+"""
+------------BIBLIOTECAS----------------
+
+Aula do dia 29/04
+"""
+
+#Conjuntos de módulos e funções pré-programadas que permitem adicionar códigos sem realmente escrevê-lo do zero
+
+#pode importar a biblioteca todo ou somente uma função dela
+import math       #biblioteca de matematica
+
+#exemplos de uso
+print(math.sqrt(16))    #Função de raiz quadrada
+
+import datetime #mecher com o tempo
+                          
+agora = datetime.now()
