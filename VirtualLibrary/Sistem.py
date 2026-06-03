3df4e6c81206edc2641b52fefe34baad16339dde
Curso <Curso@Lab7-30.Ensino.lib>
2026-06-03 17:31:46 -0300

    Adicionando o novo arquivo que irei fazer o desafio da aula de hoje!

---
+++ VirtualLibrary/Sistem.py
@@ -0,0 +1,14 @@
+"""
+-------------------------Virtual Library--------------------------------  
+
+Develop a book registration program, including title, author, genre, number of pages, and a summary.
+Using Python, SQLite, and CustomTkinter.
+In the main window, there will be only two buttons: one to register a book and another to list all books.
+
+
+Author: Vinícius Albino Klug Fagundes
+Date: 03/06/2026
+
+*************************************************************************
+In this file, you will se all the graphic interface code and the program logic
+"""
